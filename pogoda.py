67bb4699df13f955c95ed17ed0066451f55fffcd
Klepikov Dmitriy <thebrootos@gmail.com>
2021-04-27 07:38:18 +0500

    update 27

--- pogoda.py
+++ pogoda.py
@@ -1,9 +1,18 @@
 import requests
-from bs4 import BeautifulSoup
 from PyQt5 import QtWidgets
 from pogodaUI import Ui_MainWindow
 import sys
-import time
+
+
+weather_parameters = {
+    '0': '',
+    'T': ''
+    }
+
+request_headers = {
+    'Accept-Language': 'ru'
+}
+
 
 listkz = ['Алматы', 'Нур-Султан', 'Астана', 'Шымкент', 'Актобе', 'Караганда', 'Тараз', 'Павлодар', 'Усть-Каменогорск', 'Семей',
           'Атырау', 'Костанай', 'Кызылорда', 'Уральск', 'Петропавловск', 'Актау', 'Темиртау', 'Туркестан',
@@ -26,18 +35,13 @@ class mywindow(QtWidgets.QMainWindow):
 
     def action(self):
         content = self.ui.lineEdit_city.text()
-        if content not in listkz:            
-            return self.ui.label_info.setText('Введите город Казахстана, например Алматы')
+        if content not in listkz:
+            return self.ui.text_out.setText('Введите город Казахстана, например Алматы')
         else:
-            search = "Температура в"
-            url = "https://www.google.com/search?q={0}".format(content)
-            r = requests.get(url)
-            s = BeautifulSoup(r.text, "html.parser")
-            update = s.find("div", class_="BNeawe").text
-            named_tuple = time.localtime()
-            time_now = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
-            strings = search + " " + update + " (" + time_now + ")"
-            return self.ui.label_info.setText(strings)
+            url = f'https://wttr.in/{content}'
+            return self.ui.text_out.setText(requests.get(url,
+                                                            params=weather_parameters,
+                                                            headers=request_headers).text)
 
 if __name__ in "__main__":
     app = QtWidgets.QApplication([])
